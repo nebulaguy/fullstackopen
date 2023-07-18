2703dd622d6a288c7f9b3223e0920ced11453443
vrishank <vrish.2013@gmail.com>
2023-07-18 13:17:58 -0400

    1.3-1.5

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,28 +1,37 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+ 
 
   return (
     <div>
-      <Header name={course} />
-      <Content name={part1} value={exercises1} />
-      <Content name={part2} value={exercises2}/>
-      <Content name={part3} value={exercises3} />
-      <Total value={exercises1 + exercises2 + exercises3} />
+      <Header name={course.name} />
+      <Content p1={course.parts[0]} p2={course.parts[1]} p3={course.parts[2]}/>
+      <Total value={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
     </div>
   )
 }
 
-const Header = (course) => {
-  console.log(course) 
+const Header = (coursename) => {
+  console.log(coursename) 
   return (
     <div> 
-      <h1> {course.name} </h1>
+      <h1> {coursename.name} </h1>
     </div>
   )
 }
@@ -31,8 +40,18 @@ const Content = (content) => {
   console.log(content) 
   return (
     <div>
-      <p> {content.name} {content.value}</p>
+      <Part partname={content.p1.name} partex={content.p1.exercises}/>
+      <Part partname={content.p2.name} partex={content.p2.exercises}/>
+      <Part partname={content.p3.name} partex={content.p3.exercises}/>
     </div>
+  )
+}
+
+const Part = (part) => {
+  return (
+  <div>
+    <p>{part.partname} {part.partex}</p>
+  </div>
   )
 }
 
@@ -44,8 +63,6 @@ const Total = (total) => {
     </div>
   )
 }
-
-
 
 
 export default App
