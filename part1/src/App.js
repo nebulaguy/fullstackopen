989c13eeb34a5828b0ef8d29893b0077b3228782
vrishank <vrish.2013@gmail.com>
2023-07-26 16:29:35 -0400

    1.6->1.14

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,68 +1,64 @@
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+import { useState } from 'react'
+
+const MostVotes = (props) => {
+
+  let maxValue = props.values[0];
+  let maxIndex = 0;
+
+  for (let i = 1; i < props.values.length; i++) {
+    if (props.values[i] > maxValue) {
+      maxValue = props.values[i];
+      maxIndex = i;
+    }
   }
- 
 
   return (
     <div>
-      <Header name={course.name} />
-      <Content p1={course.parts[0]} p2={course.parts[1]} p3={course.parts[2]}/>
-      <Total value={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
+      <h1>Anecdote with most votes</h1>
+      {props.list[maxIndex]}
+      <p>has {maxValue} votes</p>
     </div>
+   
   )
 }
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  var ary = new Uint8Array(anecdotes.length) 
+  const [votes, setVotes] = useState(ary)
 
-const Header = (coursename) => {
-  console.log(coursename) 
-  return (
-    <div> 
-      <h1> {coursename.name} </h1>
-    </div>
-  )
-}
+  
+  const Random = () => {
+    const randomInt = Math.floor(Math.random() * (anecdotes.length-1))
+    setSelected(randomInt)
+  }
 
-const Content = (content) => {
-  console.log(content) 
-  return (
-    <div>
-      <Part partname={content.p1.name} partex={content.p1.exercises}/>
-      <Part partname={content.p2.name} partex={content.p2.exercises}/>
-      <Part partname={content.p3.name} partex={content.p3.exercises}/>
-    </div>
-  )
-}
+  const Vote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
 
-const Part = (part) => {
-  return (
-  <div>
-    <p>{part.partname} {part.partex}</p>
-  </div>
-  )
-}
-
-const Total = (total) => {
-  console.log(total)
   return (
     <div>
-      <p>Number of exercises {total.value}</p>
+      <h1>Anecdote of the day</h1>
+      {anecdotes[selected]}
+      <p>has {votes[selected]} votes</p>
+      <button onClick={Vote}>vote</button>
+      <button onClick={Random}>next anecdote</button>
+      <MostVotes list={anecdotes} values={votes}/>
     </div>
   )
 }
-
 
 export default App
